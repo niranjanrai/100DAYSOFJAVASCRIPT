4957576f3ecfea6a4fb6c8800548a2cc57d165d9
niranjanrai <rainiranjan1998@gmail.com>
2022-08-29 22:33:12 +0530

    updated

---
+++ 27_Aug/variable.js
@@ -0,0 +1,17 @@
+// 1. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with there type.
+
+var string = "Niranjan";
+var num = 10;
+var boolean = true;
+var undedf = undefined;
+var nul = null;
+console.log(typeof string, num, boolean, undedf, nul);
+
+// 2. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+let firstName = "Niranjan";
+let lastName = "Rai";
+let maritalStatus = "Unmarried";
+let country = "India";
+let age = "35";
+
+console.log(``);
