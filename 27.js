d89911a75cc79a73a86ee526d286395a9e5e3939
niranjanrai <rainiranjan1998@gmail.com>
2022-08-31 01:16:48 +0530

    updated

---
+++ 27.js
@@ -0,0 +1,38 @@
+// Arrays method
+// push pop
+// shift unshift
+
+let fruits = ["mango", "apple", "grapes"];
+console.log(fruits);
+
+// push method
+// push method add element from the last *
+// fruits.push("banana"); // add elemt
+// console.log(fruits);
+// if we returnthe push value it give us the lenght of the array
+// console.log(fruits.push("banana")); // give the lenghth of the array
+
+// pop method
+// pop method removes the element from the last
+// fruits.pop(); // removes
+// console.log(fruits);
+
+//it also give removes  value as return
+// let removes = fruits.pop();
+// console.log("the removed value is", removes);
+
+// shift method
+// shift method removes the value from the start
+// fruits.shift(); // removes
+// console.log(fruits);
+// it also gives return value same as pop method
+// let removed = fruits.shift();
+// console.log(fruits);
+// console.log("removed value is", removed);
+
+// unshift method
+// it add value from the start
+// fruits.unshift("banana");
+console.log(fruits);
+// it also give length as a return
+console.log(fruits.unshift("banana")); // 4
