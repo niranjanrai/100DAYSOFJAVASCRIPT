09976d2765382c5c66df01e6f65ef9bfe782215b
niranjanrai <rainiranjan1998@gmail.com>
2022-08-31 02:50:35 +0530

    updated

---
+++ 30.js
@@ -0,0 +1,12 @@
+// for loop in array
+let fruits = ["apple", "mango", "grapes", "banana"];
+// console.log(fruits);
+// console.log(fruits.length);
+// console.log(fruits[fruits.length - 2]);
+
+let fruits2 = [];
+
+for (i = 0; i < fruits.length; i++) {
+  fruits2.push(fruits[i].toUpperCase());
+}
+console.log(fruits2);
