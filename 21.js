9b0ea437d9b27694ac3dd97db26affa457411a5f
niranjanrai <rainiranjan1998@gmail.com>
2022-08-30 14:17:03 +0530

    updated

--- 21.js
+++ 21.js
@@ -1,6 +1,22 @@
-console.log(this)
-console.log(window)
-var firstName;
-firstName = "Niranjan"
-console.log(firstName)
+// while loops example
+// let num = 10;
+// let total = 0;
+// total = total + 1;
+// total = total + 2;
+// total = total + 3;
+// total = total + 4;
+// total = total + 5;
+// total = total + 6;
+// total = total + 7;
+// total = total + 8;
+// total = total + 9;
+// total = total + 10;
+// console.log(total);
 
+let i = 0;
+let total = 0;
+while (i <= 200) {
+  total = total + i;
+  i++;
+}
+console.log(total);
