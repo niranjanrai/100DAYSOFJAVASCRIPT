3cd1e01f8ba7f421bda7daf8b00b2d24ca04e1cc
niranjanrai <rainiranjan1998@gmail.com>
2022-08-31 03:58:32 +0530

    updated

---
+++ 36.js
@@ -0,0 +1,14 @@
+// Object
+
+// Arrays are good but not sufficient in real world data
+// Object store key value pair
+// Object dont have index value
+// objec is a reference type
+
+// how to declare a object
+const person = {
+  name: "Nir",
+  age: 22,
+  profession: "coder",
+}; // also known as object literalls
+console.log(person);
