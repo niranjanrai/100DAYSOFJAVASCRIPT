5ff9dd476f2b6c746839f784243770950ea64abf
niranjanrai <rainiranjan1998@gmail.com>
2022-09-02 15:23:56 +0530

    updated

--- 49.js
+++ 49.js
@@ -1,1 +1,22 @@
-50;
+// Lexical Scope
+// const myVar = "inside global scope";
+
+function myApp() {
+  const myVar = "Value1";
+
+  function myFunc() {
+    // const myVar = "Value50"; // if commented then print value1
+
+    const myFunc2 = () => {
+      console.log("inside myFunc2,", myVar);
+    };
+    // console.log("Inside from myFunc", myVar);
+    myFunc2();
+  }
+  //   const myFunc2 = function () {};
+  //   const myFunc3 = () => {};
+  //
+  console.log(myVar);
+  myFunc();
+}
+myApp();
