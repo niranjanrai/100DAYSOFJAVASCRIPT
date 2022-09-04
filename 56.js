f436364d4b6074c2585a8456618945e7a54d507a
niranjanrai <rainiranjan1998@gmail.com>
2022-09-05 00:45:00 +0530

    updated

--- 56.js
+++ 56.js
@@ -1,6 +1,15 @@
+"use strict";
 //  set
-let data = new Set(["Nir", "Chandan", "Manoj", "Shravan", "Deepak"]);
-console.log(data);
-data.add("Juber");
-console.log(data);
+// let data = new Set(["Nir", "Chandan", "Manoj", "Shravan", "Deepak"]);
+// console.log(data);
+// data.add("Juber");
+// console.log(data);
 // ......
+
+console.log(this);
+const calcAge = function (birthYear) {
+  console.log(2030 - birthYear);
+  console.log(this);
+};
+
+calcAge(1998);
