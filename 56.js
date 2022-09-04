18860d52ed8d8b4531ec58b4b9486e90ebd5fb9a
niranjanrai <rainiranjan1998@gmail.com>
2022-09-05 00:57:44 +0530

    updated

--- 56.js
+++ 56.js
@@ -6,20 +6,29 @@
 // console.log(data);
 // ......
 
-console.log(this);
-const calcAge = function (birthYear) {
-  console.log(2030 - birthYear);
-  console.log(this);
-};
+// console.log(this);
+// const calcAge = function (birthYear) {
+//   console.log(2030 - birthYear);
+//   console.log(this);
+// };
 
-calcAge(1998);
+// calcAge(1998);
 
 //  Arrow function
 
 // console.log(this);
-const calcAgeArrow = (birthYear) => {
-  console.log(2030 - birthYear);
-  console.log(this);
-};
+// const calcAgeArrow = (birthYear) => {
+//   console.log(2030 - birthYear);
+//   console.log(this);
+// };
 
-calcAgeArrow(1988);
+// calcAgeArrow(1988);
+
+const nir = {
+  year: 1998,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+nir.calcAge();
