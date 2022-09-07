94fd4de6fcc99f1b973f9bb9eff91d1562497c11
niranjanrai <rainiranjan1998@gmail.com>
2022-09-08 02:05:41 +0530

    first commit

---
+++ 39.js
@@ -0,0 +1,16 @@
+//  Computed Value
+
+const key1 = "objKey1";
+const key2 = "objKey2";
+
+const value2 = "myValue1";
+const value1 = "myValue2";
+
+const obj = {
+  [key1]: value1,
+  [key2]: value2,
+};
+
+// obj[key1] = value1;
+// obj[key2] = value2;
+console.log(obj);
