69219dc09673567212ef907b55691bd81625e328
niranjanrai <rainiranjan1998@gmail.com>
2022-09-08 01:53:08 +0530

    first commit

--- 38.js
+++ 38.js
@@ -5,3 +5,28 @@ const person1 = {
   age: 22,
   hobbies: ["sleeping", "coding", "listening music"],
 };
+
+// There are two ways of iterations
+// 1 --> for in loop
+// 2 --> object.keys
+
+// for in loop
+
+for (let key in person1) {
+  //   console.log(key); // for key
+  //   console.log(person1[key]); // for value
+  //   console.log(`${key} : ${person1[key]} `); // for both
+  //   console.log(key, ":", person1[key]); // more
+}
+
+// Object.keys
+
+// console.log(typeof Object.keys(person1));
+// console.log(Array.isArray(Object.keys(person1))); // for checking it is array or not
+
+for (const key of Object.keys(person1)) {
+  //   console.log(key); // for key
+  //   console.log(person1[key]); // for value
+  //   console.log(` ${key} : ${person1[key]} `); // for both
+  console.log(key, ":", person1[key]); // for both
+}
