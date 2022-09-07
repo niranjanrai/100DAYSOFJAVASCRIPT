a598b754db3883b76bb28b7ff1e34e08bb83b074
niranjanrai <rainiranjan1998@gmail.com>
2022-09-08 01:10:16 +0530

    first commit

--- 36.js
+++ 36.js
@@ -12,3 +12,38 @@ const person = {
   profession: "coder",
 }; // also known as object literalls
 console.log(person);
+
+// ....................
+// {} --> Object literals
+//  new Object() --> object constructor
+// ....................
+
+//  how to access data
+console.log(person.name);
+console.log(person.age);
+
+// Also store array in object
+const person1 = {
+  name: "Manoj",
+  age: 22,
+  hobbies: ["sleeping", "coding", "listening music"],
+};
+
+console.log(person1);
+
+// how to key value pair in object
+
+person1.gender = "male";
+console.log(person1);
+
+// Bracket notation
+console.log(person1["name"]);
+console.log(person1["age"]);
+console.log(person1["hobbies"]);
+
+// Add key value with the help of bracket notation
+person1["profession"] = "Coder"; // always use string in bracket
+console.log(person1);
+
+// some facts
+//  --object key are always in string
--- 55.js
+++ 55.js
@@ -90,8 +90,6 @@ function myFunc() {
 }
 myFunc();
 
-const myFunction = () => {
-  
-};
+const myFunction = () => {};
 
 // myFunc(myFunc2);
