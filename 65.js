7222b3b1653764e1f79185c7b92828a028929bf7
niranjanrai <rainiranjan1998@gmail.com>
2022-09-28 16:02:57 +0530

    first commit

--- 65.js
+++ 65.js
@@ -1,2 +1,16 @@
 // Set Method
 // It is Itrable
+// Store data
+// set also have its own method
+// Order is not guaranteed like array
+// Only store unique item [not allowed duplicated]
+
+// Contrcuctor
+
+const number = new Set(); // Constructor
+
+// ...............
+
+// store data
+const num = new Set([1, 2, 3, 4]);
+console.log(num);
