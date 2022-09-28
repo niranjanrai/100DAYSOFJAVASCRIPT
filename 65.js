6ccbcde64a7ca0cfa64d92268b8642d1bcaa1cd5
niranjanrai <rainiranjan1998@gmail.com>
2022-09-28 16:25:25 +0530

    first commit

--- 65.js
+++ 65.js
@@ -12,5 +12,20 @@ const number = new Set(); // Constructor
 // ...............
 
 // store data
-const num = new Set([1, 2, 3, 4]);
-console.log(num);
+// const num = new Set([1, 2, 3, 4]);
+const num = new Set("Niranjan");
+// console.log(num);
+
+let key1 = null;
+let value1 = "10";
+const obj = {
+  firstName: "Manoj",
+  profession: "Manager",
+  [key1]: value1,
+};
+
+// console.log(obj);
+
+for (let num in obj) {
+  console.log(num);
+}
