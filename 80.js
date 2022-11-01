989b09d50d1fc98d22f67cfb0dfab68c6bbc3b1e
niranjanrai <rainiranjan1998@gmail.com>
2022-11-01 12:07:07 +0530

    first commit

---
+++ 80.js
@@ -0,0 +1,59 @@
+// Nullish coalescing Operator
+
+"use strict";
+
+const restuarant = {
+  name: "paramount",
+  location: "surat, gujarat",
+  categories: ["Italian", "pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruchetta", "Garlic-bread", "Caprese salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // open 24 hour
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelevery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Ordered recieved ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delevered to ${address} at ${time}  `
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delecious pasta with ${ing1}, ${ing2}, and  ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+restuarant.numGuest = 0;
+
+const guest = restuarant.numGuest || 10;
+console.log(guest);
+
+// Nullish: Null and undefined (NOT 0 or '')
+
+const correctGuest = restuarant.numGuest ?? 10;
+console.log(correctGuest);
