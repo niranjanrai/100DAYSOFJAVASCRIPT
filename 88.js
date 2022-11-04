85ff4e2bc23fc271a0d5a71123f85ff690b2192c
niranjanrai <rainiranjan1998@gmail.com>
2022-11-04 13:38:06 +0530

    first commit

--- 88.js
+++ 88.js
@@ -46,6 +46,69 @@ const restuarant = {
   },
 };
 
-const rest = new Map();
-rest.set("firstName", "Niranjan");
-console.log(rest);
+// const rest = new Map();
+// rest.set("firstName", "Niranjan");
+// rest.set(1, "India");
+// rest.set(2, "UK");
+// rest.set([1, 2, 3, 4, 5, 6], "UK");
+// console.log(rest.set({ firstName: "Niranjan" }, "UK"));
+// console.log(rest);
+// rest.set("name", "Kailash parbat");
+// rest
+//   .set("categories", ["Italian", "pizzeria", "Vegetarian", "Organic"])
+//   .set("open", 11)
+//   .set("close", 23)
+//   .set(true, "we are open :D")
+//   .set(false, "we are closed :(");
+
+// console.log(rest.get("name"));
+// console.log(rest.get(true));
+// console.log(rest.get(false));
+
+// const time = 10;
+// console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+// console.log(rest.has("categories"));
+// console.log(rest.delete(2));
+// console.log(rest);
+// console.log(rest.size);
+// console.log(rest.clear());
+// console.log(rest);
+// console.log(rest.set(document.querySelector("h1"), "heading"));
+
+// const arr = [1, 2];
+// rest.set(arr, "test");
+
+// console.log(rest.get(arr));
+// console.log(rest);
+
+const question = new Map([
+  ["question", "What is the best best programming language in the world"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "Javascript"],
+  ["correct", 3],
+  ["correct", 3],
+  [true, "Correct 🎉"],
+  [false, "Try again!"],
+]);
+
+// Convert Object to Map
+console.log(Object.entries(openingHours));
+const hourMap = new Map(Object.entries(openingHours));
+console.log(hourMap);
+console.log(question);
+3;
+
+// Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(` Answer${key}: ${value} `);
+}
+3;
+
+const answer = Number(prompt("Your answer"));
+
+// answer print
+
+console.log(question.get(question.get("correct") === answer));
