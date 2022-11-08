555cacdc2542eb9f02e9c432cad4827237bbc6c0
niranjanrai <rainiranjan1998@gmail.com>
2022-11-08 23:45:20 +0530

    first commit

---
+++ 92.js
@@ -0,0 +1,12 @@
+// Explore
+const airline = "Air India";
+console.log(airline.valueOf());
+console.log(airline.toUpperCase());
+console.log(airline.toLocaleLowerCase());
+
+// fixed the capitalization
+
+const passenger = "NirAnjAN"; // Niranjan
+const passengerLower = passenger.toLocaleLowerCase();
+console.log(passengerLower);
+console.log(passenger);
