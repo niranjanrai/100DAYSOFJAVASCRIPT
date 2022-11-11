2fe39784711f3bdfda0ef0bdea1853cdcb2d73d7
niranjanrai <rainiranjan1998@gmail.com>
2022-11-11 20:45:13 +0530

    first commit

--- 92.js
+++ 92.js
@@ -35,3 +35,34 @@ const announcement =
 
 console.log(announcement);
 console.log(announcement.replace("door", "gate"));
+console.log(announcement.replaceAll("door", "gate"));
+
+// Regex
+
+console.log(announcement.replace(/door/g, "gate"), "----> Regex");
+
+// return boolean
+const plane = "Air320neo";
+console.log(plane.includes("Air320"));
+console.log(plane.includes("Boeing"));
+console.log(plane.startsWith("Air"));
+console.log(plane.startsWith("320"));
+
+if (plane.startsWith("Air") && plane.endsWith("neo")) {
+  console.log("this is correct plane");
+}
+
+// practice
+const checkBaggae = function (items) {
+  // const baggage = items.toLowerCase();
+  const baggage = items;
+  if (baggage.includes("knife") || baggage.includes("gun")) {
+    console.log("You are not allowed at board");
+  } else {
+    console.log("Welcome on Board");
+  }
+};
+
+checkBaggae("i have a laptop, some foods and a pocket Knife ");
+checkBaggae("Snacks and Camera ");
+checkBaggae("Got some snacks and gun for bored");
