b03c5e4f9bf0df1399ec0540150b8d06158767ab
niranjanrai <rainiranjan1998@gmail.com>
2022-11-12 00:59:59 +0530

    first commit

--- 92.js
+++ 92.js
@@ -67,7 +67,7 @@ checkBaggae("i have a laptop, some foods and a pocket Knife ");
 checkBaggae("Snacks and Camera ");
 checkBaggae("Got some snacks and gun for bored");
 
-console.log(" ----------> Classes <-----------");
+console.log("----------> Classes <-----------");
 
 class Rectangle {
   constructor(height, width) {
@@ -77,6 +77,68 @@ class Rectangle {
 }
 
 console.log(Rectangle.name);
-
 const here = new Rectangle(10, 10);
 console.log(here);
+
+// split and Join method string
+
+console.log("a+very+nice+string".split("+"));
+console.log("Niranjan Rai".split(" "));
+
+const [firstName, lastName] = "Niranjan Rai".split(" ");
+console.log(firstName, lastName); // magic
+
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalized = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+  // console.log(names);
+  for (let n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+    console.log(namesUpper.join(" "), "----> ans");
+  }
+};
+
+capitalized("Manoj and shravan");
+capitalized("Niranjan Rai");
+capitalized("aayushi");
+capitalized("shubham");
+capitalized("great work");
+
+// Padding
+const message = " Go to gate 23!";
+console.log(message.padStart(2500, "^ "), message.padEnd(2500, "* "));
+
+// Real world example
+
+const maskedCredit = function (number) {
+  const str = number + "";
+  const last = str.slice(-4);
+  return last.padStart(20, "*");
+  return last.padStart(str.length, "*");
+};
+
+console.log(maskedCredit(2554818));
+console.log(maskedCredit(7600944655));
+console.log(maskedCredit(7016644209)); // Number
+console.log(maskedCredit(15456423132454356));
+console.log(maskedCredit("21585152124663126565"));
+
+// .........Rectangle............Rectangle.prototype...Rectangle.........Rectangle.........Rectangle......Rectangle..............
+
+// Repeat
+const message2 = "Bad weather...., All Departures cancelled / Delayed.....";
+// console.log(message2.repeat(51));
+// console.log(message3.repeat());
+
+//
+const planeInLine = function (n) {
+  console.log(`There are ${n} planes are waiting ${"✈️".repeat(n)}`);
+};
+
+planeInLine(1);
+planeInLine(6);
+planeInLine(15);
