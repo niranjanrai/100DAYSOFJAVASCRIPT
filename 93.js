d706073a1606a2d6664f15b7c3c01434d3965b21
niranjanrai <rainiranjan1998@gmail.com>
2022-11-13 21:25:19 +0530

    first commit

---
+++ 93.js
@@ -0,0 +1,29 @@
+// Function
+// Default parameter
+
+"use strict";
+
+const bookings = [];
+
+const createBooking = function (
+  flightNumb = 1,
+  numPassenger = 1,
+  price = 5999 * numPassenger
+) {
+  // Old way
+  //   numPassenger = numPassenger || 1;
+  //   price = price || 1;
+
+  const booking = {
+    flightNumb,
+    numPassenger,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("A320", 200, 10000);
+createBooking("A320", 200);
+createBooking("A320", 5);
